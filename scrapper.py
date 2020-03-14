056985d0fecaad67e35dd62062067b85260f8c7a
Boaz Bibi <boazbibi@gmail.com>
2020-03-15 01:48:25 +0200

    Begining to get it...

--- scrapper.py
+++ scrapper.py
@@ -2,9 +2,13 @@ import requests
 import lxml
 from lxml import html
 
-r = requests.get('http://gun.io')
+URL = "https://www.amazon.com/Happy-Haystack-Stainless-Bottle-Design/product-reviews/B07YL48NZQ/&reviewerType=all_reviews"
+
+r = requests.get(URL, headers={"User-Agent":"Defined"})
 tree = lxml.html.fromstring(r.content)
-print('\n',tree.keys(),'\n')
-elements = tree.get_element_by_id('class')
-for el in elements:
-    print(el.text_content())
+
+hrefs = tree.xpath('//span[@class="review-text"]//a/@href')
+print(hrefs)
+
+# for element in tree.iter():
+#     print("%s - %s" % (element.tag, element.text))
