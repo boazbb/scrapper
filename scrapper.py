900cff7d40b00143dbe8f123eb4f7e01a732eee4
Boaz Bibi <boazbibi@gmail.com>
2020-03-26 00:18:31 +0200

    Basic Navigation Skeleton

--- scrapper.py
+++ scrapper.py
@@ -7,8 +7,8 @@ URL = "https://www.amazon.com/Happy-Haystack-Stainless-Bottle-Design/product-rev
 URL = "https://www.amazon.com/Sleepwish-Blanket-Cartoon-Pattern-Blankets/product-reviews/B075L8PXM1/ref=cm_cr_arp_d_viewpnt_lft?ie=UTF8&reviewerType=all_reviews"
 XPATH = '//span[@class="a-size-base review-text review-text-content"]'
 
-def get_all_reviews():
-    page = requests.get(URL, headers={"User-Agent":"Defined"})
+def get_all_reviews(url):
+    page = requests.get(url, headers={"User-Agent":"Defined"})
     doc = lxml.html.fromstring(page.content)
     text = ""
 
@@ -34,4 +34,36 @@ def text_to_adj_dict(text):
     # return sortrd by value:
     return {k: v for k, v in sorted(adj_dict.items(), reverse=True, key=lambda item: item[1])}
 
-print(text_to_adj_dict(get_all_reviews()))
+SEARCH_URL = "https://www.amazon.com/s?k="
+
+class Navigator():
+    def __init__(self, product_name):
+        product_name = product_name.replace(' ', '+')
+        url = SEARCH_URL + product_name
+        page = requests.get(URL, headers={"User-Agent":"Defined"})
+        print(page.content)
+        self.html = lxml.html.fromstring(page.content)
+
+    def get_products(self, n):
+        """
+        get the first n (or maximum) products in search results
+        :param int n: The number of webpages to return
+        :return: a list of webpages
+        """
+        pass
+
+    def get_review_pages(self, product_webpage):
+        """
+        return a list of URLs of the product
+        """
+        get_all_reviews
+
+    def get_adj_dict(self, review_pages):
+        text = ""
+        for page in review_pages:
+            text += get_all_reviews(page)
+        return text_to_adj_dict(text)
+
+if __name__ == "__main__":
+    nav = Navigator("miami memory")
+    # TODO: write get_products
