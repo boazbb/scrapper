f7d1edb5ba9085ef432f3109d9a23489d87bd014
Boaz Bibi <boazbibi@gmail.com>
2020-03-31 21:28:17 +0300

    Finished with searching products

--- scrapper.py
+++ scrapper.py
@@ -2,17 +2,24 @@ import requests
 import lxml
 from lxml import html
 from nltk.corpus import wordnet as wn
+from tqdm import tqdm # for pretty progress bars
 
 URL = "https://www.amazon.com/Happy-Haystack-Stainless-Bottle-Design/product-reviews/B07YL48NZQ/&reviewerType=all_reviews"
 URL = "https://www.amazon.com/Sleepwish-Blanket-Cartoon-Pattern-Blankets/product-reviews/B075L8PXM1/ref=cm_cr_arp_d_viewpnt_lft?ie=UTF8&reviewerType=all_reviews"
-XPATH = '//span[@class="a-size-base review-text review-text-content"]'
+
+SEARCH_URL = "https://www.amazon.com/s?k={SEARCH_TERM}&page={PAGE_NUM}"
+AMAZON_PREFIX = "https://www.amazon.com"
+
+PRODUCT_XPATH = '//a[@class="a-link-normal a-text-normal"]'
+REVIEW_BUTTON_XPATH = '//a[@class="a-link-emphasis a-text-bold"]'
+REVIEW_TEXT_XPATH = '//span[@class="a-size-base review-text review-text-content"]'
 
 def get_all_reviews(url):
     page = requests.get(url, headers={"User-Agent":"Defined"})
     doc = lxml.html.fromstring(page.content)
     text = ""
 
-    review_spans = doc.xpath(XPATH)
+    review_spans = doc.xpath(REVIEW_TEXT_XPATH)
     for rev in review_spans:
         for i in rev:
             text += str(i.text) + " "
@@ -34,15 +41,9 @@ def text_to_adj_dict(text):
     # return sortrd by value:
     return {k: v for k, v in sorted(adj_dict.items(), reverse=True, key=lambda item: item[1])}
 
-SEARCH_URL = "https://www.amazon.com/s?k="
-
 class Navigator():
     def __init__(self, product_name):
-        product_name = product_name.replace(' ', '+')
-        url = SEARCH_URL + product_name
-        page = requests.get(URL, headers={"User-Agent":"Defined"})
-        print(page.content)
-        self.html = lxml.html.fromstring(page.content)
+        self.product_name = product_name.replace(' ', '+')
 
     def get_products(self, n):
         """
@@ -50,7 +51,24 @@ class Navigator():
         :param int n: The number of webpages to return
         :return: a list of webpages
         """
-        pass
+        products_urls = []
+        i = 1
+        while(len(products_urls) < n):
+            url = SEARCH_URL.format(SEARCH_TERM=self.product_name, PAGE_NUM=i)
+            page = requests.get(url, headers={"User-Agent":"Defined"})
+            self.doc = lxml.html.fromstring(page.content)
+            products_a = self.doc.xpath(PRODUCT_XPATH)
+            print(f"In page {i} there are {len(products_a)} products.")
+            if (len(products_a) == 0):
+                # This means we reached a page without products
+                break 
+            for p in products_a:
+                if len(products_urls) > n: break # If we already have n products we can return
+                if not (p.get('href') in products_urls): # For some reason we get duplicates sometime
+                    products_urls.append(p.get('href'))
+            i += 1
+        print(f"found {len(products_urls)} items.")
+        return products_urls
 
     def get_review_pages(self, product_webpage):
         """
@@ -65,5 +83,12 @@ class Navigator():
         return text_to_adj_dict(text)
 
 if __name__ == "__main__":
-    nav = Navigator("miami memory")
-    # TODO: write get_products
+    nav = Navigator("table")
+    products_urls = nav.get_products(20)
+    all_review = ""
+    for prod_url in tqdm(products_urls):
+        rev_url = prod_url.replace("/dp/", "/product-review/")
+        all_review += (" "+get_all_reviews(AMAZON_PREFIX+rev_url))
+    adj_dict = text_to_adj_dict(all_review)
+    print(adj_dict)
+    
