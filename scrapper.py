55dbee1e1222fef9941c7fe645a25ea874488415
Boaz Bibi <boazbibi@gmail.com>
2020-04-03 17:08:13 +0300

    Now reading all review pages

--- scrapper.py
+++ scrapper.py
@@ -14,17 +14,24 @@ PRODUCT_XPATH = '//a[@class="a-link-normal a-text-normal"]'
 REVIEW_BUTTON_XPATH = '//a[@class="a-link-emphasis a-text-bold"]'
 REVIEW_TEXT_XPATH = '//span[@class="a-size-base review-text review-text-content"]'
 
-def get_all_reviews(url):
-    page = requests.get(url, headers={"User-Agent":"Defined"})
-    doc = lxml.html.fromstring(page.content)
-    text = ""
+headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7',
+            'cache-control': 'no-cache',
+            'pragma': 'no-cache',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        }
 
-    review_spans = doc.xpath(REVIEW_TEXT_XPATH)
-    for rev in review_spans:
-        for i in rev:
-            text += str(i.text) + " "
-    
-    return text
+# hpc is health and household. just add &i={} in the url
+categories = ["arts-crafts", "automotive", "baby-products", "beauty", "stripbooks", "computers", "digital-music", "electronics", "digital-text",
+                "instant-video", "fashion-womens", "fashion-mens", "fashion-girls", "fashion-boys", "deals", "hpc", "kitchen", "industrial", 
+                "luggage", "movies-tv", "music", "pets", "software", "sporting", "tools", "toys-and-games", "videogames"]
+
 
 def text_to_adj_dict(text):
     text = text.lower()
@@ -55,7 +62,7 @@ class Navigator():
         i = 1
         while(len(products_urls) < n):
             url = SEARCH_URL.format(SEARCH_TERM=self.product_name, PAGE_NUM=i)
-            page = requests.get(url, headers={"User-Agent":"Defined"})
+            page = requests.get(url, headers=headers)
             self.doc = lxml.html.fromstring(page.content)
             products_a = self.doc.xpath(PRODUCT_XPATH)
             print(f"In page {i} there are {len(products_a)} products.")
@@ -70,29 +77,49 @@ class Navigator():
         print(f"found {len(products_urls)} items.")
         return products_urls
 
-    def get_review_pages(self, product_webpage):
+    def get_all_reviews_from_page(self, url):
+        page = requests.get(url, headers=headers)
+        doc = lxml.html.fromstring(page.content)
+        text = ""
+
+        review_spans = doc.xpath(REVIEW_TEXT_XPATH)
+        for rev in review_spans:
+            for i in rev:
+                text += str(i.text) + " "
+    
+        return text
+
+    def get_product_review_text(self, product_webpage):
         """
         return a list of URLs of the product
         """
-        get_all_reviews
+        page_suffix = "&pageNumber={num}"
+        text = self.get_all_reviews_from_page(product_webpage)
+        i = 2
+        new_text = self.get_all_reviews_from_page(product_webpage+page_suffix.format(num=i))
+        while(new_text != ""):
+            print(f"i is {i}")
+            text += new_text
+            i += 1
+            new_text = self.get_all_reviews_from_page(product_webpage+page_suffix.format(num=i))
+        return text
 
-    def get_adj_dict(self, review_pages):
-        text = ""
-        for page in review_pages:
-            text += get_all_reviews(page)
-        return text_to_adj_dict(text)
 
 if __name__ == "__main__":
     nav = Navigator("table")
-    products_urls = nav.get_products(20)
+    products_urls = nav.get_products(1)
     all_review = ""
     for prod_url in tqdm(products_urls):
         rev_url = prod_url.replace("/dp/", "/product-review/")
-        all_review += (" "+get_all_reviews(AMAZON_PREFIX+rev_url))
+        all_review += (" "+nav.get_product_review_text(AMAZON_PREFIX+rev_url))
     adj_dict = text_to_adj_dict(all_review)
     print(adj_dict)
 
 
-#TODO: Add Review Text w. different dictionaries
+#TODO: Think of a better structure
+#TODO: Add Product Information Text w. different dictionaries
 #TODO: And Spacy/or better nltk
-#TODO: Add Navigating Review Pages
+#TODO: Saving the url of the products, with a dict per URL
+#TODO: Adding search by category
+#TODO: In the dictionary, save a pointer to the original text
+#TODO: Add option to export (use argparser)
