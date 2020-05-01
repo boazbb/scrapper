ac7d0fc03441ea8b345a9e214637b1c9830a9e04
Boaz Bibi <boazbibi@gmail.com>
2020-05-01 14:27:03 +0300

    Adding arguemnt parser, and dumping the dictionary etc...

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,8 @@ import lxml
 from lxml import html
 from nltk.corpus import wordnet as wn
 from tqdm import tqdm # for pretty progress bars
+import argparse
+import json
 
 URL = "https://www.amazon.com/Happy-Haystack-Stainless-Bottle-Design/product-reviews/B07YL48NZQ/&reviewerType=all_reviews"
 URL = "https://www.amazon.com/Sleepwish-Blanket-Cartoon-Pattern-Blankets/product-reviews/B075L8PXM1/ref=cm_cr_arp_d_viewpnt_lft?ie=UTF8&reviewerType=all_reviews"
@@ -13,6 +15,7 @@ AMAZON_PREFIX = "https://www.amazon.com"
 
 PRODUCT_XPATH = '//a[@class="a-link-normal a-text-normal"]'
 REVIEW_BUTTON_XPATH = '//a[@class="a-link-emphasis a-text-bold"]'
+REVIEW_COUNT_XPATH = '//span[@data-hook="cr-filter-info-review-count"]'
 REVIEW_TEXT_XPATH = '//span[@class="a-size-base review-text review-text-content"]'
 
 headers = {
@@ -25,13 +28,35 @@ headers = {
             'sec-fetch-site': 'same-origin',
             'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         }
 
 # hpc is health and household. just add &i={} in the url
 categories = ["arts-crafts", "automotive", "baby-products", "beauty", "stripbooks", "computers", "digital-music", "electronics", "digital-text",
                 "instant-video", "fashion-womens", "fashion-mens", "fashion-girls", "fashion-boys", "deals", "hpc", "kitchen", "industrial", 
                 "luggage", "movies-tv", "music", "pets", "software", "sporting", "tools", "toys-and-games", "videogames"]
+
+REVIEW_IN_PAGE = 10
+
+def main():
+    args = parse_args()
+    if (args.list_categories):
+        # Print all possible categories
+        print('All possible categories:')
+        print('  '+'\n  '.join(categories))
+        exit(0)
+    nav = Navigator(args.keyword, args.category)
+    products = nav.get_products(args.pages)
+    all_review = ""
+    for prod in tqdm(products):
+        rev_url = prod.url.replace("/dp/", "/product-review/")
+        print(f"The URL we are looking for:\n{AMAZON_PREFIX+rev_url}\n")
+        all_review += (" "+nav.get_product_review_text(AMAZON_PREFIX+rev_url))
+    adj_dict = text_to_adj_dict(all_review)
+    print(adj_dict)
+    if args.dump:
+        dump_file = open(args.dump, 'w')
+        dump_file.write(json.dumps(adj_dict))
 
 
 def text_to_adj_dict(text):
@@ -101,10 +126,12 @@ class Navigator():
                 break 
             for p in products_a:
                 if len(products_urls) > n: break # If we already have n products we can return
-                if not (p.get('href') in products_urls): # For some reason we get duplicates sometime
+                # For some reason we get duplicates sometime, also another search result k=SEARCHTERM
+                if not (p.get('href') in products_urls) and not p.get('href').startswith('/s?k='):
                     products_urls.append(p.get('href'))
             i += 1
         print(f"found {len(products_urls)} items.")
+        print(products_urls)
         return Product.url_list_to_product_list(products_urls)
 
     def get_all_reviews_from_page(self, url):
@@ -123,31 +150,53 @@ class Navigator():
         """
         return a list of URLs of the product
         """
+        # Find the overall number of reviews:
+        page = requests.get(product_webpage, headers=headers)
+        doc = lxml.html.fromstring(page.content)
+        count = int(doc.xpath(REVIEW_COUNT_XPATH)[0].text.split()[-2].replace(',',''))
+        page_number = - (-count // REVIEW_IN_PAGE) # Rounding Up
+        
         page_suffix = "&pageNumber={num}"
-        text = self.get_all_reviews_from_page(product_webpage)
-        i = 2
-        new_text = self.get_all_reviews_from_page(product_webpage+page_suffix.format(num=i))
-        while(new_text != ""):
-            print(f"i is {i}")
-            text += new_text
-            i += 1
+        text = ''
+        for i in tqdm(range(1, page_number+1)):
+        # Get the review text:
             new_text = self.get_all_reviews_from_page(product_webpage+page_suffix.format(num=i))
+            text += new_text
         return text
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '-l', '--list_categories',
+        help='Use this flag to list all possible categories.',
+        action='store_true'
+        )
+    group.add_argument(
+        '-k', '--keyword',
+        help='The keyword to search for'
+        )
+    parser.add_argument(
+        '-c', '--category',
+        help='The amazon category to search in',
+        default=None
+        )
+    parser.add_argument(
+        '-p', '--pages',
+        help='Maximal number of search pages to go through',
+        type=int, default=1000
+        )
+    parser.add_argument(
+        '-d', '--dump',
+        help='Save the dictionary to dump_file (using JSON)',
+        metavar="dump_file", default=None
+        )
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    nav = Navigator("table", "kitchen")
-    products = nav.get_products(1)
-    all_review = ""
-    for prod in tqdm(products):
-        rev_url = prod.url.replace("/dp/", "/product-review/")
-        all_review += (" "+nav.get_product_review_text(AMAZON_PREFIX+rev_url))
-    adj_dict = text_to_adj_dict(all_review)
-    print(adj_dict)
+    main()
 
-#TODO: Continue adding category support (for now it seems taht when using categories it doesn't extract reviews)
-
-#TODO: Use argparser for options
 #TODO: Add Product Information Text w. different dictionaries
 #TODO: And Spacy/or better nltk
 #TODO: Saving the url of the products, with a dict per URL
